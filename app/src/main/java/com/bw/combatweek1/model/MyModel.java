2b7e43f92956c301592b99b585450bef762ff867
Gaomengjun <2326318806@qq.com>
2019-12-02 16:02:50 +0800

    提交

--- app/src/main/java/com/bw/combatweek1/model/MyModel.java
+++ app/src/main/java/com/bw/combatweek1/model/MyModel.java
@@ -3,7 +3,7 @@ package com.bw.combatweek1.model;
 import com.bw.combatweek1.contract.IMyContract;
 import com.bw.combatweek1.util.NetUtil;
 import com.google.gson.Gson;
-//dichsb
+
 public class MyModel {
     public void getMyData(final IMyContract.IModelCallback iModelCallback) {
         NetUtil.getInstance().getJson("http://blog.zhaoliang5156.cn/api/shop/shop1.json", new NetUtil.MyCallback() {
